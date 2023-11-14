9b852602bf0c9f70091839eea4ba1f48fcf096e6
kanhaiya kumar <kk7160464@gmail.com>
2023-11-15 00:03:15 +0530

    weather app

---
+++ weatherApp/weather.js
@@ -0,0 +1,28 @@
+
+
+
+const apikey = "a75df97c3c1d0c21e4f3dd769036b7c9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkweather(searchBox.value);
+})
